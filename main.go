d642bf9b6bcbd10dff501f8c7d39febccbe3a0a2
abhishek <abhishekpathania.407@gmail.com>
2021-01-19 20:03:56 +0530

    connecting controller endpoints, repository & usecase into main.go

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"goServer/adapters"
+	"goServer/app/controller"
+	"goServer/app/repository"
+	"goServer/app/usecase"
 	"goServer/config"
 	"log"
 	"net/http"
@@ -10,7 +13,11 @@ import (
 func main() {
 	serveMux := http.NewServeMux()
 	conf := config.GetConfig()
-	adapters.NewDBAdapterRepository(conf)
+	dbconn := adapters.NewDBAdapterRepository(conf)
+
+	toDorp := repository.NewToDoRepo(dbconn)
+	toDouc := usecase.NewToDoUsecase(toDorp)
+	controller.NewTodo(serveMux, toDouc)
 
 	log.Println("listening on port:", conf.HttpConfig.HostPort)
 	if err := http.ListenAndServe(conf.HttpConfig.HostPort, serveMux); err != nil {
